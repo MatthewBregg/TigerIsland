76fdcb624e554a05da3cac9a33273f60d2171cdb
jsbrown <jsbrown@ufl.edu>
2017-03-16 14:23:43 -0400

    Merged Location.java and Orientation.java from branch 'Joshua' to update their dependencies

--- main/src/Location.java
+++ main/src/Location.java
@@ -1,25 +1,87 @@
-import javafx.geometry.Point3D;
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
+import java.util.Objects;
 
-/**
- * Created by josh on 3/15/17.
- */
-public class Location {
+public class Location 
+{
+	private int x;
+	private int y;
+	private int z;
 
-    private Point3D coordinates;
+	public Location(int x, int y, int z)
+	{
+		this.x = x;
+		this.y = y;
+		this.z = z;
 
-    public Location(int x, int y, int z) {
-        coordinates = new Point3D(x,y,z);
-    }
+		if(x+y+z!=0)
+		{
+			throw new InvalidParameterException("x+y+z must equal zero");
+		}
+	}
 
-    public int getX(){
-        return (int) coordinates.getX();
-    }
+	public Location(int x, int y)
+	{
+		this(x,y,-(x+y));
+	}
 
-    public int getY(){
-        return (int) coordinates.getY();
-    }
+	public int getX() {
+		return x;
+	}
 
-    public int getZ(){
-        return (int) coordinates.getZ();
-    }
+	public int getY() {
+		return y;
+	}
+
+	public int getZ() {
+		return z;
+	}
+
+	public Location getAdjacent(Orientation md)
+	{
+		int newX = x + md.getDx();
+		int newY = y + md.getDy();
+		int newZ = z + md.getDz();
+
+		return new Location(newX, newY, newZ);
+	}
+
+
+	public ArrayList<Location> getSurroundingLocations()
+	{
+		ArrayList<Location> locs=new ArrayList<Location>();
+		Orientation[] mds = {Orientation.getSouthEast(), Orientation.getEast(), 
+				Orientation.getSouthWest(), Orientation.getNorthWest(), 
+				Orientation.getWest(), Orientation.getNorthEast()};
+
+		Location loc=this;
+
+		for(int i=0; i<mds.length; i++)
+		{
+			locs.add(loc);
+			loc = loc.getAdjacent(mds[i]);
+		}
+
+		return locs;
+	}
+
+	public boolean equals(Object o)
+	{
+		if(!(o instanceof Location))
+		{
+			return false;
+		}
+
+		Location target = (Location)o;
+		return x == target.x && y == target.y && z == target.z;
+	}
+
+	public int hashCode(){
+		return Objects.hash(x, y);
+	}
+
+	public String toString()
+	{
+		return "("+ x +", " + y + ", " + z + ")";
+	}
 }
