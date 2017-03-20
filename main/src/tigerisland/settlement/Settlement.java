c7507512ab93b4a061738db42b19ef36c21427be
mbregg <matthewbregg@gmail.com>
2017-03-20 17:34:48 -0400

    Implemented interface for settlements

---
+++ main/src/tigerisland/settlement/Settlement.java
@@ -0,0 +1,10 @@
+package tigerisland.settlement;
+
+import tigerisland.board.Location;
+import tigerisland.piece.Piece;
+
+import java.util.Map;
+
+public class Settlement {
+    Map<Location,Piece> piecesInSettlement;
+}
