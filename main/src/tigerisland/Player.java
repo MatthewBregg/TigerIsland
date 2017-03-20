8b2cb55cfa262cd33a1ff0cd955a1a51740b137e
Joshua Novick <jnovick@ufl.edu>
2017-03-20 00:56:06 -0400

    Add Player and Tests

--- main/src/tigerisland/Player.java
+++ main/src/tigerisland/Player.java
@@ -1,31 +1,62 @@
 package tigerisland;
 
-public class Player {
-	private PlayerID id;
-	private int villagerCount;
-	private int totoroCount;
-	
-	public Player(int villagerCount, int totoroCount)
-	{
-		id=new PlayerID();
-		this.villagerCount=villagerCount;
-		this.totoroCount=totoroCount;
-	}
-	
-	public Player()
-	{
-		this(20,3);
-	}
+public class Player
+{
+    private PlayerID id;
+    private int villagerCount;
+    private int totoroCount;
 
-	public PlayerID getId() {
-		return id;
-	}
+    public Player(int villagerCount, int totoroCount)
+    {
+        id = new PlayerID();
+        this.villagerCount = villagerCount;
+        this.totoroCount = totoroCount;
+    }
 
-	public int getVillagerCount() {
-		return villagerCount;
-	}
+    public Player()
+    {
+        this(20, 3);
+    }
 
-	public int getTotoroCount() {
-		return totoroCount;
-	}
+    public PlayerID getId()
+    {
+        return id;
+    }
+
+    public int getVillagerCount()
+    {
+        return villagerCount;
+    }
+
+    public int getTotoroCount()
+    {
+        return totoroCount;
+    }
+
+    public boolean removeVillagers(int quantity)
+    {
+        if(villagerCount < quantity)
+        {
+            return false;
+        }
+
+        villagerCount -= quantity;
+        return true;
+    }
+
+    public boolean removeVillager()
+    {
+        return removeVillagers(1);
+    }
+
+    public boolean removeTotoro()
+    {
+        if(totoroCount == 0)
+        {
+            return false;
+        }
+
+        totoroCount--;
+        return true;
+    }
 }
