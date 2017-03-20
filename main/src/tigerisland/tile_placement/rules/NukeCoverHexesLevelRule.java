b2a78ce98e0f2e6ce9601575d0f340d3de8c047d
carlosdarienvizcaino <carlos.vizcaino@ufl.edu>
2017-03-20 16:26:36 -0400

    Adds rules package

---
+++ main/src/tigerisland/tile_placement/rules/NukeCoverHexesLevelRule.java
@@ -0,0 +1,14 @@
+package tigerisland.tile_placement.rules;
+
+
+import tigerisland.Location;
+import tigerisland.Tile;
+import tigerisland.tile_placement.TilePlacement;
+
+public class NukeCoverHexesLevelRule implements TilePlacement {
+
+    @Override
+    public void placeTile(Tile tile, Location location) throws Throwable {
+
+    }
+}
---
+++ main/src/tigerisland/tile_placement/rules/NukeHexesMustBeOfDifferentTilesRule.java
@@ -0,0 +1,13 @@
+package tigerisland.tile_placement.rules;
+
+import tigerisland.Location;
+import tigerisland.Tile;
+import tigerisland.tile_placement.TilePlacement;
+
+public class NukeHexesMustBeOfDifferentTilesRule implements TilePlacement {
+
+    @Override
+    public void placeTile(Tile tile, Location location) throws Throwable {
+
+    }
+}
