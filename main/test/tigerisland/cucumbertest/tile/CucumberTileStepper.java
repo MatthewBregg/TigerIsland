f67c9b0a865105fe0c8247f3b5014826f55bdba1
mbregg <matthewbregg@gmail.com>
2017-03-23 21:59:01 -0400

    fixed the tile examining cucumber tests

--- main/test/tigerisland/cucumbertest/tile/CucumberTileStepper.java
+++ main/test/tigerisland/cucumbertest/tile/CucumberTileStepper.java
@@ -1,10 +1,10 @@
 package tigerisland.cucumbertest.tile;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.junit.Assert;
+import tigerisland.hex.Hex;
 import tigerisland.terrains.Jungle;
 import tigerisland.terrains.Rocky;
 import tigerisland.terrains.Terrain;
@@ -12,16 +12,13 @@ import tigerisland.terrains.Volcano;
 import tigerisland.tile.Orientation;
 import tigerisland.tile.Tile;
 
-/**
- * Created by mbregg on 3/16/17.
- */
 public class CucumberTileStepper {
     public Tile tile = null;
     @Given("^We can examine a tile$")
     public void weCanExamineATile() throws Throwable {
        Terrain lTerrain = Rocky.getInstance();
        Terrain rTerrain = Jungle.getInstance();
-       tile = new Tile(0, Orientation.getEast());
+       tile = new Tile(0, new Hex(Volcano.getInstance()), new Hex(lTerrain), new Hex(rTerrain));
     }
 
     @When("^We examine the left and right portions of said tile$")
