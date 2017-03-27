5ff3f5ea51157c467e37b1c4d6899812d8571522
jsbrown <jsbrown@ufl.edu>
2017-03-27 17:29:34 -0400

    New rules, some refactorings

---
+++ main/src/tigerisland/build_moves/rules/NotEnoughTigersRule.java
@@ -0,0 +1,11 @@
+package tigerisland.build_moves.rules;
+
+import tigerisland.build_moves.builds.BuildActionData;
+import tigerisland.build_moves.builds.BuildActionResult;
+
+public class NotEnoughTigersRule implements BuildActionRule {
+    public BuildActionResult applyRule(BuildActionData buildActionData){
+        return null;
+    }
+
+}
