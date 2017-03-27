44992f96a47ae04ff24d8a30bf4b7a5466cd5f62
jsbrown <jsbrown@ufl.edu>
2017-03-27 19:44:17 -0400

    Some new rules and renamings. Will be adding more

---
+++ main/src/tigerisland/build_moves/rules/PlayerMustHaveATigerToBuildRule.java
@@ -0,0 +1,12 @@
+package tigerisland.build_moves.rules;
+
+import tigerisland.build_moves.builds.BuildActionData;
+import tigerisland.build_moves.builds.BuildActionResult;
+
+public class PlayerMustHaveATigerToBuildRule implements BuildActionRule {
+    public BuildActionResult applyRule(BuildActionData buildActionData){
+        return null;
+    }
+
+}
+
