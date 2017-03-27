d23fd287766ff12aeb35f445d73015729e40817e
PGA <philipa@ufl.edu>
2017-03-27 02:48:40 -0400

    Changes and additions to threaded server code

--- main/src/tigerislandserver/server/TournamentServer.java
+++ main/src/tigerislandserver/server/TournamentServer.java
@@ -3,16 +3,18 @@ package tigerislandserver.server;
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
-import java.util.concurrent.*;
 
 /**
  * Created by pgallen on 3/26/2017.
  */
 public class TournamentServer {
-    private ServerSocket serverSocket = null;
-    ArrayList<Socket> clientConnections = new ArrayList<>();
+    private ServerSocket serverSocket;
+    private ArrayList<TournamentClient> clientConnections = new ArrayList<>();
+    private boolean currentlyAcceptingConnections;
 
     public TournamentServer(){
+        currentlyAcceptingConnections = false;
+
         try{
             serverSocket = new ServerSocket(0);
         } catch(IOException e){
@@ -28,11 +30,57 @@ public class TournamentServer {
         return portNumber;
     }
 
-    public void getConnections(){
-        // TODO: threaded client connection implementation
+    public void acceptConnections(int connectionTimeout_s){
+        // Running accept() on separate thread should allow socket.close() to be called from this thread
+        // timeout argument is in milliseconds, converted to milliseconds
+        if(currentlyAcceptingConnections)
+            return;
+
+        currentlyAcceptingConnections = true;
+
+        connectionTimeout_s *= 1000;
+
+        new Thread(new ConnectionAcceptor(connectionTimeout_s)).start();
     }
 
     public void finalize(){
         // TODO: close all connections
     }
+
+    class ConnectionAcceptor implements Runnable{
+        int connectionAcceptanceTimeout;
+
+        ConnectionAcceptor(int connectionTimeout){
+            connectionAcceptanceTimeout = connectionTimeout;
+        }
+
+        public void run(){
+            try{
+                serverSocket.setSoTimeout(connectionAcceptanceTimeout);
+            }catch(SocketException e){
+                System.out.println("Connection protocol error on port " + getPort());
+            }
+
+            boolean listening = true;
+            while(listening){
+                try{
+                    Socket newClient = serverSocket.accept();
+                    TournamentClient client =
+                            new TournamentClient(newClient);
+
+                    synchronized (clientConnections){
+                        clientConnections.add(client);
+                    }
+
+                    Thread configureClient = new Thread(client);
+                    configureClient.start();
+                } catch(IOException e){
+                    System.out.println("Exception caught or problem listening on port " + getPort());
+                }
+            }
+
+            currentlyAcceptingConnections = false;
+        }
+    }
 }
+
