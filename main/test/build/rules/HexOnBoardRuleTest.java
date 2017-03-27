210b841006eb5372f096578d0506fe8c0ae4b263
carlosdarienvizcaino <carlos.vizcaino@ufl.edu>
2017-03-27 17:09:11 -0400

    Adds hex on board rule

---
+++ main/test/build/rules/HexOnBoardRuleTest.java
@@ -0,0 +1,73 @@
+package build.rules;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import tigerisland.board.Board;
+import tigerisland.board.HexBoard;
+import tigerisland.board.Location;
+import tigerisland.build.BuildActionData;
+import tigerisland.build.BuildActionResult;
+import tigerisland.build.rules.BuildActionRule;
+import tigerisland.build.rules.HexOnBoardRule;
+import tigerisland.hex.Hex;
+import tigerisland.player.Player;
+
+public class HexOnBoardRuleTest {
+
+    private BuildActionRule hexOnBoardRule;
+    private Board board;
+
+    @Before
+    public void setup() {
+
+        this.board = new HexBoard();
+        this.hexOnBoardRule = new HexOnBoardRule(board);
+    }
+
+    @Test
+    public void test_ShouldReturnUnsuccessfulWhenHexIsNotOnBoard() {
+
+        // Arrange
+        final String EMPTY_HEX_ERROR_MESSAGE = "Hex is NOT empty";
+
+        Player player = new Player();
+        Location hexLocation = new Location(0, 0, 0);
+
+        BuildActionData buildActionData = new BuildActionData.Builder()
+                .withPlayer(player)
+                .withHexLocation(hexLocation)
+                .build();
+
+
+        // Act
+        BuildActionResult result = hexOnBoardRule.applyRule(buildActionData);
+
+        // Assert
+        Assert.assertFalse(result.successful);
+        Assert.assertEquals(EMPTY_HEX_ERROR_MESSAGE, result.errorMessage);
+    }
+
+    @Test
+    public void test_ShouldReturnSuccessfulWhenPieceBoardAtLocationIsNotUsed() {
+
+        // Arrange
+        final String EMPTY_HEX_ERROR_MESSAGE = "Hex is not on board";
+        Player player = new Player();
+        Location hexLocation = new Location(0, 0, 0);
+
+        BuildActionData buildActionData = new BuildActionData.Builder()
+                .withPlayer(player)
+                .withHexLocation(hexLocation)
+                .build();
+
+        Hex hex = new Hex();
+        board.placeHex(hexLocation, hex);
+
+        // Act
+        BuildActionResult result = hexOnBoardRule.applyRule(buildActionData);
+
+        // Assert
+        Assert.assertTrue(result.successful);
+    }
+}
