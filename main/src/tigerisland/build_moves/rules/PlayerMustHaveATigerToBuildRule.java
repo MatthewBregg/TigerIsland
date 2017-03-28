440806b2a2d1d7121467445a42becde603b8370c
jsbrown <jsbrown@ufl.edu>
2017-03-28 19:17:17 -0400

    Added Must Have Tiger Rule

--- main/src/tigerisland/build_moves/rules/PlayerMustHaveATigerToBuildRule.java
+++ main/src/tigerisland/build_moves/rules/PlayerMustHaveATigerToBuildRule.java
@@ -2,11 +2,18 @@ package tigerisland.build_moves.rules;
 
 import tigerisland.build_moves.builds.BuildActionData;
 import tigerisland.build_moves.builds.BuildActionResult;
+import tigerisland.player.Player;
 
 public class PlayerMustHaveATigerToBuildRule implements BuildActionRule {
-    public BuildActionResult applyRule(BuildActionData buildActionData){
-        return null;
+
+    private final String MUST_HAVE_TIGER_ERROR = "Player must have at least one tiger to do this build";
+    private BuildActionResult successfulResult = new BuildActionResult(true);
+    private BuildActionResult failedResult = new BuildActionResult(false, MUST_HAVE_TIGER_ERROR);
+
+
+    @Override
+    public BuildActionResult applyRule(BuildActionData buildActionData) {
+       Player player = buildActionData.getPlayer();
+       return player.getTigerCount() > 0 ? successfulResult : failedResult;
     }
-
 }
-
