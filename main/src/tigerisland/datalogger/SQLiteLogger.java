0ddfb5c7484ab276d74903dc5616450b69b8702a
mbregg <matthewbregg@gmail.com>
2017-04-04 15:27:33 -0400

    Created a method to set up the database in the SqlliteLogger. We can have this called by whatever is creating the logger, and then just use a new database file for each tourney.

--- main/src/tigerisland/datalogger/SQLiteLogger.java
+++ main/src/tigerisland/datalogger/SQLiteLogger.java
@@ -4,10 +4,7 @@ import tigerisland.board.Location;
 import tigerisland.player.PlayerID;
 import tigerisland.tile.Orientation;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class SQLiteLogger implements DataLogger {
     private int challengeId;
@@ -182,7 +179,23 @@ public class SQLiteLogger implements DataLogger {
         ++turnNumber;
     }
 
-    public Connection getConnection() {
-        return connection;
+
+    public void createTables() {
+        final String[] queries = new String[]{
+                "CREATE TABLE IF NOT EXISTS matches (challenge_id integer not null, game_id integer not null, p1_id integer not null, p2_id integer not null, status string, primary key(challenge_id, game_id) );",
+                "CREATE TABLE IF NOT EXISTS tiles_placed (challenge_id integer not null, game_id integer not null, turn_number integer not null, p_id integer not null, loc_x integer not null, loc_y integer not null, loc_z integer not null, orientation integer not null, tile text not null, primary key(challenge_id, game_id, turn_number) );",
+                "CREATE TABLE IF NOT EXISTS build_action (challenge_id integer not null, game_id integer not null, turn_number integer not null, p_id integer not null, loc_x integer not null, loc_y integer not null, loc_z integer not null, move_description text not null, primary key(challenge_id, game_id, turn_number) );",
+                "CREATE TABLE IF NOT EXISTS invalid_moves (challenge_id integer not null, game_id integer not null, turn_number integer not null, p_id integer not null, message string not null, primary key(challenge_id, game_id, turn_number) );",
+                "create table IF NOT EXISTS raw_requests ( time_stamp integer primary key, request text not null);"
+        };
+
+        try {
+            for ( String query : queries ) {
+                Statement stmt = connection.createStatement();
+                stmt.execute(query);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
