d9c59ae9e6ed17a00e21b2b5e7e654a7cfdd029c
mbregg <matthewbregg@gmail.com>
2017-04-10 00:46:56 -0400

    Added java scoreboard generator

---
+++ main/src/tigerislandserver/scoreboard/ScoreBoardMain.java
@@ -0,0 +1,46 @@
+package tigerislandserver.scoreboard;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import static java.lang.Thread.sleep;
+
+public class ScoreBoardMain {
+    private final static String FILENAME = "./ScoreBoard.html";
+    public static void main(String[] args) {
+        while(true) {
+            GenerateScoreBoard scoreBoardGenerator = new GenerateScoreBoard("jdbc:sqlite:tigersssss.db");
+            String scoreboard = (scoreBoardGenerator.getScoreBoard());
+            BufferedWriter bufferedWriter = null;
+            FileWriter fileWriter = null;
+            try {
+                fileWriter = new FileWriter(FILENAME);
+                bufferedWriter = new BufferedWriter(fileWriter);
+                bufferedWriter.write(scoreboard);
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (bufferedWriter != null) {
+                        bufferedWriter.close();
+                    }
+                    if (fileWriter != null) {
+                        fileWriter.close();
+                    }
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+            try {
+                sleep(50000);
+            } catch (Exception ex) {
+                System.out.print(ex);
+            }
+        }
+    }
+
+
+}
