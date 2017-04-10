a4449c89fa5f3ee12f4b6bbba00ccf96c38b5308
mbregg <matthewbregg@gmail.com>
2017-04-10 02:16:43 -0400

    Added a way to get just datbase logger

--- main/src/tigerisland/datalogger/LoggerFactory.java
+++ main/src/tigerisland/datalogger/LoggerFactory.java
@@ -7,10 +7,17 @@ public class LoggerFactory {
 
     synchronized public static DataLogger getLogger(int gameid, int challengeid, int matchid) {
             DataLogger consoleLogger = new ConsoleLogger(challengeid,gameid,matchid);
-            SQLiteLogger sqlLogger = new SQLiteLogger(challengeid,gameid,matchid,dataBaseUrl);
-            sqlLogger.createTables();
+            SQLiteLogger sqlLogger = LoggerFactory.getSQLLogger(gameid,challengeid,matchid);
             return new CompositeLogger(sqlLogger,consoleLogger);
-        }
+    }
+
+    synchronized public static SQLiteLogger getSQLLogger(int gameid, int challengeid, int matchid) {
+        SQLiteLogger sqlLogger = new SQLiteLogger(challengeid,gameid,matchid,dataBaseUrl);
+        sqlLogger.createTables();
+        return sqlLogger;
+
+    }
+
 
     public static String getDataBaseUrl() {
         return dataBaseUrl;
