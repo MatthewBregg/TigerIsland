fc5654cb651f1d732c0b76708ed948a569b74372
mbregg <matthewbregg@gmail.com>
2017-04-11 14:38:05 -0400

    Attempted to make turns still in lock step, but take less than time out.

---
+++ main/src/tigerislandserver/gameplay/TurnSynchronizer.java
@@ -0,0 +1,24 @@
+package tigerislandserver.gameplay;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TurnSynchronizer {
+    List<GameThread> games = new ArrayList<>();
+    public void addGame(GameThread game) {
+        games.add(game);
+    }
+
+    synchronized public void checkGamesReadyToMove() {
+        for (GameThread game : games ) {
+            if ( !game.hasTurnWaiting() ) {
+                return;
+            }
+        }
+
+        // All games are ready to move
+        for ( GameThread game : games ) {
+            game.makeMove();
+        }
+    }
+}
