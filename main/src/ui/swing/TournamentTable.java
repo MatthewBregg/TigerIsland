bde052986e1257142e770b1fb284f7f225d9ce1e
carlosdarienvizcaino <carlos.vizcaino@ufl.edu>
2017-04-14 14:54:35 -0400

    Adds scores to labels

--- main/src/ui/swing/TournamentTable.java
+++ main/src/ui/swing/TournamentTable.java
@@ -22,8 +22,8 @@ public class TournamentTable {
 
     private String[] tournamentColumnHeaders = {
             "Team Names",
-            "Tourney",
-            "Challenge",
+            "Tourney Score",
+            "Challenge Score",
             "Opponent"};
 
     private String[] gameAColumnHeaders = {
